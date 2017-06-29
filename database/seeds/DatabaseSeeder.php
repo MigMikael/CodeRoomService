1521341848f179d455be0da0da6a852c05fc1011
Chanachai Puttaruksa <Chanachai_mig@hotmail.com>
2017-06-29 16:38:49 +0700

    edit some seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(FileTableSeeder::class);
-        $this->call(StudentTableSeeder::class);
+        //$this->call(StudentTableSeeder::class);
         $this->call(CourseTableSeeder::class);
         //$this->call(StudentCourseTableSeeder::class);
         $this->call(LessonTableSeeder::class);
