c1f09af8b239fc737c66af2763f01411abc4a50f
Chanachai Puttaruksa <Chanachai_mig@hotmail.com>
2017-09-19 16:18:16 +0700

    fix bug calstructure score again

--- app/Http/Controllers/ProblemController.php
+++ app/Http/Controllers/ProblemController.php
@@ -324,14 +324,14 @@ class ProblemController extends Controller
                 $mets = $pA['methods'];
                 foreach ($mets as $met){
                     $method = ProblemMethod::findOrFail($met['id']);
-                    if ($met['recursive'] == null){
+                    if ($met['recursive'] == 'null'){
                         $met['recursive'] = 'false';
                     }else{
                         $met['recursive'] = 'true';
                     }
                     $method->recursive = $met['recursive'];
 
-                    if ($met['loop'] == null){
+                    if ($met['loop'] == 'null'){
                         $met['loop'] = 'false';
                     }else{
                         $met['loop'] = 'true';
