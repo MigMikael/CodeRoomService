f09a1f015e88c67acb1672d7d4457e3cc56996be
Chanachai Puttaruksa <Chanachai_mig@hotmail.com>
2017-10-27 16:20:47 +0700

    handle wrong submit file again again

--- app/Http/Controllers/SubmissionController.php
+++ app/Http/Controllers/SubmissionController.php
@@ -971,6 +971,11 @@ class SubmissionController extends Controller
                     }else{
                         $correct = false;
                         $result_attr = new ResultAttribute;
+                        $result_attr->result_id = $result->id;
+                        $result_attr->access_modifier = 'wrong';
+                        $result_attr->non_access_modifier = 'wrong';
+                        $result_attr->data_type = 'wrong';
+                        $result_attr->name = 'wrong';
                     }
 
                     if($correct){
@@ -1024,6 +1029,10 @@ class SubmissionController extends Controller
                     }else{
                         $is_correct = false;
                         $result_con = new ResultConstructor;
+                        $result_con->result_id = $result->id;
+                        $result_con->access_modifier = 'wrong';
+                        $result_con->name = 'wrong';
+                        $result_con->parameter = 'wrong';
                     }
 
                     if($is_correct){
@@ -1070,6 +1079,14 @@ class SubmissionController extends Controller
                     } else {
                         $is_correct = false;
                         $result_me = new ResultMethod;
+                        $result_me->result_id = $result->id;
+                        $result_me->access_modifier = 'wrong';
+                        $result_me->non_access_modifier = 'wrong';
+                        $result_me->return_type = 'wrong';
+                        $result_me->name = 'wrong';
+                        $result_me->parameter = 'wrong';
+                        $result_me->recursive = 'wrong';
+                        $result_me->loop = 'wrong';
                     }
 
                     if ($is_correct){
